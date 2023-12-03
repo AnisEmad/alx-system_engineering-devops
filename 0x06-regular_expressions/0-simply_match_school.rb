0a6926921399f20647e5bf8d66f61cf696867550
Anis Emad <anisemad123@gmail.com>
2023-12-03 14:15:57 +0200

    solved first task

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+# 0-simply_match_school.rb
+
+# Check if the argument matches the regular expression
+def match_school?(input)
+    # Regular expression to match the word "School"
+    puts ARGV[0].scan(/School/).join
+  end
+  
+  # Get the command line argument
+  input_argument = ARGV[0]
+  
+  # Call the method to match the regular expression
+  match_school?(input_argument)
